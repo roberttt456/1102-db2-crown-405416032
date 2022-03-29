6cfff3a742e49fdf85292d9784d3d59ae1d67429
roberttt456 <wongkabb2003@hotmail.com>
2022-03-29 16:17:31 +0800

    week6 database add

--- routes/jackets.js
+++ routes/jackets.js
@@ -1,11 +1,11 @@
+/*
 var express = require('express');
+
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('crown2_32/index', { id: '405416032',name:'黃嘉雄' });
+    res.render('crown2_32/jackets', { id: '405416032',name:'黃嘉雄' });
 });
 
-
-
 module.exports = router;
+*/
---
+++ utils/database.js
@@ -0,0 +1,25 @@
+const {Pool} = require('pg');
+const {connectionString} = require("pg/lib/defaults");
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+let pool;
+
+if(isProduction){
+    pool=new pool(
+        {
+            connectionString: process.env.DATEBASE_URL,
+            ssl: {rejectUnauthorized: false}
+        }
+    )
+}else {
+    pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        port: '5432',
+        database: 'crown_32',
+        password: '0000'
+    })
+}
+
+module.exports = pool;
