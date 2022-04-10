1d17867685f0b831fed72208495316cd587a64b0
roberttt456 <wongkabb2003@hotmail.com>
2022-04-11 00:57:50 +0800

    hw1 final

--- utils/database.js
+++ utils/database.js
@@ -14,7 +14,7 @@ if(isProduction){
     )
 }else {
     // local database
-
+/*
     pool =  new Pool({
         user: 'postgres',
         host: 'localhost',
@@ -22,10 +22,10 @@ if(isProduction){
         database: 'crown_405416032',
         password: '0000'
     })
-
+*/
 
     // heroku database
-/*
+
     pool = new Pool({
         user: 'puwwcmzwgmvklz',
         host: 'ec2-54-83-21-198.compute-1.amazonaws.com',
@@ -33,7 +33,7 @@ if(isProduction){
         database: 'd5dsfn88cf4qjb',
         password: '668148c3f348c307ececaa8d527a196e6fb176e71c3603cfb413e03c30303ee1@'
     })
-*/
+
 }
 
 module.exports = pool;
