2f08ab58140c3d76023559af155870fde350ad3e
roberttt456 <wongkabb2003@hotmail.com>
2022-04-12 16:59:20 +0800

    w08 P1:model Shop_xx and test fetchProductsByCategory(id)

--- models/shop_32.js
+++ models/shop_32.js
@@ -2,13 +2,14 @@ const db = require('../utils/database');
 
 const shop_32 = class shop_32{
     //資料庫
-    constructor(id, name, size, remote_url, local_url) {
+    constructor(id, name, size, remote_url, local_url,price,cat_id) {
         this.id = id;
         this.name = name;
         this.size = size;
+        this.price=price;
         this.remote_url = remote_url;
         this.local_url = local_url;
-
+        this.cat_id=cat_id;
     }
 
     //get all categories
@@ -22,18 +23,29 @@ const shop_32 = class shop_32{
         }catch (err){
             console.log('error',err)
         }
+    }
 
-
+    static async fetchPro(id){
+        const query = {
+            text:`SELECT * FROM shop_32 where cat_id = $1`,
+            values:[id]
+        }
+        try{
+            let results = await  db.query(query);
+            return results.rows;
+        }catch (err){
+            console.log(err);
+        }
     }
 }
 
-//測試程式
-const test = async function(){
 
-    let results = await shop_32.fetchAll();
+//測試程式
+const test = async() =>{
+    let results = await shop_32.fetchPro(1);
     console.log('test results',JSON.stringify(results));
 }
 
-//test();
+test();
 
 module.exports = shop_32;
