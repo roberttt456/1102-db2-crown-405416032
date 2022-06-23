1c8844fe373ded9c7e9090fc8f01258521f924c6
roberttt456 <wongkabb2003@hotmail.com>
2022-06-23 11:26:30 +0800

    w17 git

--- routes/book_32.js
+++ routes/book_32.js
@@ -46,4 +46,61 @@ router.post('/create', async (req, res) => {
 
 });
 
+router.get('/delete/:id', async (req, res) => {
+    try {
+        console.log('delete id', req.params.id);
+        const query = {
+            text: `DELETE
+                   FROM book_32
+                   WHERE id = $1`,
+            values: [req.params.id],
+        };
+        const results = await db.query(query);
+        res.redirect('/book_32');
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log('id',id);
+    try {
+        const query = {
+            text: `SELECT * FROM book_32 WHERE id =$1`,
+            values: [id],
+        };
+        const results = await db.query(query);
+        data = results.rows;
+        res.render('book_32/edit_32',{
+            id: data[0].id,
+            name: data[0].name,
+            author: data[0].author,
+            price: data[0].price,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+router.post('/update', async (req, res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+    const author = req.body.author;
+    const price = req.body.price;
+
+    try {
+        const query = {
+            text: `UPDATE book_32 SET name = $1, author =$2, price=$3 WHERE id = $4`,
+            values: [id, name, author, price],
+        }
+        await db.query(query);
+        res.redirect('/book_32');
+    } catch (e) {
+        console.log(e)
+    }
+
+
+});
+
 module.exports = router;
